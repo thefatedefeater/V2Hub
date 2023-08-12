0052e00dad1072de29399af8e50d0bd901507e0f
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2023-08-12 04:33:54 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,5 @@
 <?php
 
 $subscription_urls = [
-  "https://alienvpn402.github.io/AlienVPN402-subscribe-servers/",
-  "https://branch.blanku.me/",
-  "https://raw.githubusercontent.com/irancpi/Subscription/main/Sub",
-  "https://raw.githubusercontent.com/amirmohammad-mohammad-88/Sub-Reality-Azadi-config/Config/Azadi-Reality-Different",
-  "https://sublink.cpionline.co/zaku68HqhhMq/93ed0514-854b-4e6c-9967-f8dc4b40db8a/all.txt?name=sublink-MCI&asn=MCI&mode=new",
-  "https://amghezi.duckdns.org/zaku68HqhhMq/93ed0514-854b-4e6c-9967-f8dc4b40db8a/all.txt?name=drctsublink-unknown&asn=unknown&mode=new",
-  "https://nika4.hdfy2.foriran.trade/sqjC9UQotFfQNr6bHWuKrJ/79770a32-9607-4919-9483-0f1794559390/all.txt?name=direct-unknown&asn=unknown&mode=new",
-  "https://nika3hdfy2.foriran.trade/sqjC9UQotFfQNr6bHWuKrJ/79770a32-9607-4919-9483-0f1794559390/all.txt?name=cdnall-unknown&asn=unknown&mode=new",
-  "https://raw.githubusercontent.com/IranianCypherpunks/sub/main/config",
+  "https://raw.githubusercontent.com/coldwater-10/Free-V2ray-Config/main/All_Configs_Sub.txt",
 ];
