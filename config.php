70f22fd4948722f42d3378bb2d64f43411daa8f3
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2023-08-22 16:17:20 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 $subscription_urls = [
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector/main/vless_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector/main/mixed_iran.txt",
 ];
