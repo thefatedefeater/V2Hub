9443814e6ba691361b175ae2dc240fcdf8492de2
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2023-08-22 20:42:47 +0330

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,4 @@
 $subscription_urls = [
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollector/main/vless_iran.txt",
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollector/main/ss_iran.txt",
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector/main/trojan_iran.txt",
 ];
